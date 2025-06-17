2474413bd142a694684d65ec14ec39ed2029a141
Vitor Mollo Cunha <cunhamollovitor10@hotmail.com>
2025-06-17 00:38:19 -0300

    V0.095
    Mais alguns ajustes básicos e rápidos na aplicação

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import AssistidoTeste, Cliente
+
+
+admin.site.register(AssistidoTeste)
+admin.site.register(Cliente)
--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    context = {
+        'curso': 'programa blablabla'
+    }
+    return render(request,"index.html", context)
+
+def login(request):
+    return render(request, "login.html")
