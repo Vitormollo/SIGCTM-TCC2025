60800774fb3a9df6ad51724661aa0221203b9869
Vitor Mollo Cunha <cunhamollovitor10@hotmail.com>
2025-06-18 17:55:08 -0300

    v0.1.37
    Ajuste da raiz, funcionalidade no logout e o lembre de mim

--- SIGCTM/urls.py
+++ SIGCTM/urls.py
@@ -20,8 +20,9 @@ from django.urls import path
 from core import views
 
 urlpatterns = [
+    path('', views.home_view, name='home'),
     path('admin/', admin.site.urls),
     path('login/', views.login_view, name='login'),
-    path('logout/', views.logout, name='logout'),
+    path('logout/', views.logout_view, name='logout'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,31 +1,45 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout as django_logout
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from datetime import timedelta
 from django.http import HttpResponse
 from .models import usuarios
 
-# Create your views here.
 def login_view(request):
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
+        remember_me = request.POST.get('remember_me')  # Pega o checkbox
 
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
+
+            # Se o usuário NÃO marcar "Lembre-se de mim", a sessão expira ao fechar o navegador
+            if not remember_me:
+                request.session.set_expiry(0)  # Expira ao fechar navegador
+            else:
+                # Se marcar, dura por exemplo 30 dias
+                request.session.set_expiry(timedelta(days=365))
+
             return redirect('dashboard')
         else:
-            messages.error(request,"Usuário ou senha inválidos.")
-    
-    return render(request,'login.html')
+            messages.error(request, "Usuário ou senha inválidos.")
 
+    return render(request, 'login.html')
 
-def logout(request):
-    logout(request)
+def logout_view(request):
+    django_logout(request)
     return redirect('login')
 
 @login_required(login_url='login')
 def dashboard_view(request):
     return render(request, 'index.html')
+
+def home_view(request):
+    if request.user.is_authenticated:
+        return redirect('dashboard')
+    else:
+        return redirect('login')
